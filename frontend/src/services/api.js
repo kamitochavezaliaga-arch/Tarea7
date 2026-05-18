6cedabdfe0b9d0fc8f4166a106cc4f5ca0ac6d22
kamitochavezaliaga-arch <kamitochavezaliaga@gmail.com>
2026-05-17 23:27:59 -0500

    Conexión frontend con backend Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
 
-    baseURL: 'http://localhost:3000'
+    baseURL: 'baseURL: "https://tarea7-backend.onrender.com"'
 })
 
 export default api
